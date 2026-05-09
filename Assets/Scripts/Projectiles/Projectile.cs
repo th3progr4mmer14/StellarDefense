9f795bf3df4fd8f387afbd755567797f3eaf9eee
Eduardo <eduardo1mollinedo@gmail.com>
2026-05-09 13:32:02 -0600

    feat: integracion de sprites del SpaceShooterAssetPack
    - Asignados sprites a Player, enemigos (Basic/Fast/Tank), proyectiles y power-ups
    
    - Anadido fondo estrellado tileado
    
    - Creadas Wave_02, Wave_03 y Wave_04 con progresion de dificultad
    
    - Fix: rotacion dinamica de proyectiles en Projectile.cs

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -50,7 +50,7 @@ namespace StellarDefense.Projectiles
             this.speed = speed;
 
             // Orientamos el sprite hacia la dirección de vuelo (asumiendo sprite "mira" arriba).
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
 
